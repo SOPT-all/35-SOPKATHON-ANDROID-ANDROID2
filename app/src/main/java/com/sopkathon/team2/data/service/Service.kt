f67bcde429e685c124a8ac770cbcc9011e291034
wjdrjs00 <kimjk3409@gmail.com>
2024-11-24 05:08:15 +0900

    [Feat] 감자짓 생성하기 service 추가

--- app/src/main/java/com/sopkathon/team2/data/service/Service.kt
+++ app/src/main/java/com/sopkathon/team2/data/service/Service.kt
@@ -1,9 +1,19 @@
 package com.sopkathon.team2.data.service
 
+import com.sopkathon.team2.data.model.response.ResponseContentDto
 import com.sopkathon.team2.data.model.response.ResponseDummyDto
+import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface Service {
     @POST("/dummy")
     suspend fun loadPotatoes(id: Int): List<ResponseDummyDto>
+
+    @POST("/board/{userId}")
+    suspend fun postContent(
+        @Path("userId") userId: Int,
+        @Body content: String
+    ): ResponseContentDto
 }
