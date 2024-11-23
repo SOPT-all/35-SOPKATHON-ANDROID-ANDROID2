5a4db63be31152c7052e0a040036806ad74fdb33
wjdrjs00 <kimjk3409@gmail.com>
2024-11-24 08:14:52 +0900

    [Feat] 바텀시트 뷰 구현

--- app/src/main/java/com/sopkathon/team2/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/com/sopkathon/team2/presentation/ui/home/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.sopkathon.team2.presentation.ui.home
 import HomeViewModel
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -11,28 +12,44 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.sopkathon.team2.presentation.ui.bottomsheet.UserBottomSheet
+import com.sopkathon.team2.data.model.response.Users
+import com.sopkathon.team2.presentation.ui.bottomsheet.BottomSheetViewModel
+
 import com.sopkathon.team2.presentation.ui.home.component.InstagramShareScreen
 import com.sopkathon.team2.presentation.ui.profile.getLevelImage
 import com.sopkathon.team2.presentation.util.noRippleClickable
@@ -45,14 +62,19 @@ import org.sopt.and.R
 fun HomeScreen(
     modifier: Modifier = Modifier, viewModel: HomeViewModel = viewModel(),
     onNavigateToProfile: () -> Unit = {},
-    onNavigateToWrite: () -> Unit = {}
+    onNavigateToWrite: () -> Unit = {},
+    onUserClick: (Int) -> Unit = {},
 ) {
     val userInfo by viewModel.userInfo.collectAsState()
     val shareScreenVisible by viewModel.shareScreenVisible.collectAsState()
 
     var showBottomSheet by remember { mutableStateOf(false) }
 
+    val usersInfo = viewModel.usersInfo
 
+    LaunchedEffect(Unit) {
+        viewModel.getUsersInfo()
+    }
 
     Box(
         modifier = modifier
@@ -166,10 +188,128 @@ fun HomeScreen(
             onDismissRequest = {
                 showBottomSheet = false
             },
-            onUserClick = {}
+            onUserClick = onUserClick,
+            userInfo = usersInfo
         )
     }
 
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserBottomSheet(
+    viewModel: BottomSheetViewModel = viewModel(),
+    onDismissRequest: () -> Unit,
+    onUserClick: (Int) -> Unit,
+    userInfo: List<Users>
+) {
+//    LaunchedEffect(Unit) {
+//        viewModel.getUsersInfo()
+//    }
+
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        sheetState = sheetState,
+        containerColor = GAMJATheme.colors.gray10,
+    ) {
+        Column(
+            modifier = Modifier
+                .heightIn(max = 684.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "친구 목록",
+                color = GAMJATheme.colors.white,
+                style = GAMJATheme.typography.titleBold18,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 40.dp, bottom = 20.dp),
+                textAlign = TextAlign.Center
+            )
+
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 24.dp)
+            ) {
+                items(
+                    items = userInfo,
+                    key = { user -> user.userId }
+                ) { user ->
+                    ProfileItem(
+                        user = user,
+                        onUserClick = { onUserClick(user.userId)}
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileItem(
+    user: Users,
+    onUserClick: (Int) -> Unit
+) {
+    Column(
+        modifier = Modifier.noRippleClickable { onUserClick(user.userId) }
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+
+            ) {
+                Image(
+                    painter = when (user.level) {
+                        1 -> painterResource(R.drawable.gamja1)
+                        2 -> painterResource(R.drawable.gamja2)
+                        3 -> painterResource(R.drawable.gamja3)
+                        4 -> painterResource(R.drawable.gamja4)
+                        else -> painterResource(R.drawable.img_dummy)
+                    },
+                    contentDescription = "",
+                    modifier = Modifier
+                        .clip(shape = CircleShape)
+                        .size(45.dp),
+                    contentScale = ContentScale.Crop
+
+                )
+                Column {
+                    Text(
+                        text = user.nickName,
+                        style = GAMJATheme.typography.bodyMedium16,
+                        color = GAMJATheme.colors.white
+                    )
+                    Text(
+                        text = "Lv. ${user.level}",
+                        style = GAMJATheme.typography.bodyMedium12,
+                        color = GAMJATheme.colors.white
+                    )
+                }
+            }
+
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_arrow),
+                contentDescription = "",
+            )
+        }
+
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = GAMJATheme.colors.gray08,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+    }
 }
 
 @Preview
