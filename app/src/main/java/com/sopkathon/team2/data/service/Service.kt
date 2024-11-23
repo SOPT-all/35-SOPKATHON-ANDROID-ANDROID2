5561b151b424bf4bdce282a0d788368632aa07eb
hwiwon <hwhwhw74@naver.com>
2024-11-24 06:18:43 +0900

    [feat] Profile api 연동 세팅

--- app/src/main/java/com/sopkathon/team2/data/service/Service.kt
+++ app/src/main/java/com/sopkathon/team2/data/service/Service.kt
@@ -1,9 +1,16 @@
 package com.sopkathon.team2.data.service
 
 import com.sopkathon.team2.data.model.response.ResponseDummyDto
+import com.sopkathon.team2.data.model.response.ResponseProfileDto
+import retrofit2.Response
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface Service {
     @POST("/dummy")
     suspend fun loadPotatoes(id: Int): List<ResponseDummyDto>
+
+    @GET("/boards/{userId}")
+    suspend fun loadProfiles(@Path("userId") userId: Long): Response<ResponseProfileDto>
 }
