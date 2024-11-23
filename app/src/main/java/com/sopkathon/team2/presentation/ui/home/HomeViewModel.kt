e92fc7b96b135f2b678cac4587020d1b2d16e6c1
t1nm1ksun <minsuk07672@naver.com>
2024-11-24 03:30:30 +0900

    [chore] 유저 정보 API 로그 수정

--- app/src/main/java/com/sopkathon/team2/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/sopkathon/team2/presentation/ui/home/HomeViewModel.kt
@@ -1,3 +1,4 @@
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopkathon.team2.data.model.response.ResponseUserDto
@@ -13,8 +14,6 @@ class HomeViewModel : ViewModel() {
     private val _userInfo = MutableStateFlow<ResponseUserDto?>(null)
     val userInfo: StateFlow<ResponseUserDto?> = _userInfo
 
-    private val _error = MutableStateFlow<String?>(null)
-    val error: StateFlow<String?> = _error
 
     init {
         getUserInfo(1)
@@ -29,15 +28,20 @@ class HomeViewModel : ViewModel() {
                     val responseBody = response.body()
                     if (responseBody != null) {
                         _userInfo.value = responseBody.data
-                        _error.value = null
+                        Log.d(
+                            "ㅋㅋ",
+                            "Success: ${response.body()!!.status}, Data: ${responseBody.data}"
+                        )
+
                     } else {
-                        _error.value = "Response body is null"
+                        Log.d("ㅋㅋ", "Error: Response body is null")
                     }
                 } else {
-                    _error.value = "Error: ${response.code()} - ${response.message()}"
+                    Log.d("ㅋㅋ", "Error: ${response.code()} - ${response.message()}")
+
                 }
             } catch (e: Exception) {
-                _error.value = e.message
+                Log.d("ㅋㅋ", "User Info Fetched: ${e.message}")
             }
         }
     }
