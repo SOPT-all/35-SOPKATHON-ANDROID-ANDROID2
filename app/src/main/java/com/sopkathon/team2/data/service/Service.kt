eb59709e2d09db5cb4c38e47e6ec87f03f6b8426
t1nm1ksun <minsuk07672@naver.com>
2024-11-24 03:19:43 +0900

    [chore] HomeScreen 유저 정보 API 수정

--- app/src/main/java/com/sopkathon/team2/data/service/Service.kt
+++ app/src/main/java/com/sopkathon/team2/data/service/Service.kt
@@ -2,14 +2,16 @@ package com.sopkathon.team2.data.service
 
 import com.sopkathon.team2.data.model.response.ResponseDummyDto
 import com.sopkathon.team2.data.model.response.ResponseUserDto
+import com.sopkathon.team2.data.model.response.ResponseWrapper
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface Service {
     @POST("/dummy")
     suspend fun loadPotatoes(id: Int): List<ResponseDummyDto>
 
-    @GET("/user/{1}")
-    fun getUserById(userId: Long): Response<ResponseUserDto>
+    @GET("/user/{userId}")
+    suspend fun getUserById(@Path("userId") userId: Long): Response<ResponseWrapper<ResponseUserDto>>
 }
--- app/src/main/java/com/sopkathon/team2/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/sopkathon/team2/presentation/ui/home/HomeViewModel.kt
@@ -5,15 +5,16 @@ import com.sopkathon.team2.data.service.RetrofitInstance
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import retrofit2.Response
 
 class HomeViewModel : ViewModel() {
+
     private val service = RetrofitInstance.service
 
+    private val _userInfo = MutableStateFlow<ResponseUserDto?>(null)
+    val userInfo: StateFlow<ResponseUserDto?> = _userInfo
 
-    private val _userInfo = MutableStateFlow<Response<ResponseUserDto>?>(null)
-    val userInfo: StateFlow<Response<ResponseUserDto>?> = _userInfo
-
+    private val _error = MutableStateFlow<String?>(null)
+    val error: StateFlow<String?> = _error
 
     init {
         getUserInfo(1)
@@ -23,8 +24,20 @@ class HomeViewModel : ViewModel() {
         viewModelScope.launch {
             try {
                 val response = service.getUserById(userId)
-                _userInfo.value = response
+
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        _userInfo.value = responseBody.data // `data` 내부 값을 Flow에 전달
+                        _error.value = null
+                    } else {
+                        _error.value = "Response body is null"
+                    }
+                } else {
+                    _error.value = "Error: ${response.code()} - ${response.message()}"
+                }
             } catch (e: Exception) {
+                _error.value = e.message
             }
         }
     }
