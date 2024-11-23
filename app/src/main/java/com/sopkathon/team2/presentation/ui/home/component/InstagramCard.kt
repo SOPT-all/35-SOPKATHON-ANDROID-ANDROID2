494838e106cf64fedc7b28f06286da2515972975
hwiwon <hwhwhw74@naver.com>
2024-11-24 07:39:01 +0900

    [feat] instagramCard에 감자 배경 추가

--- app/src/main/java/com/sopkathon/team2/presentation/ui/home/component/InstagramCard.kt
+++ app/src/main/java/com/sopkathon/team2/presentation/ui/home/component/InstagramCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,7 +23,7 @@ import com.sopkathon.team2.ui.theme.GAMJATheme
 import org.sopt.and.R
 
 @Composable
-fun InstagramCard(nickname: String,level:Int,modifier: Modifier = Modifier) {
+fun InstagramCard(nickname: String, level: Int, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -43,15 +44,22 @@ fun InstagramCard(nickname: String,level:Int,modifier: Modifier = Modifier) {
         )
         Spacer(modifier = Modifier.height(42.dp))
 
-        val levelImage =getLevelImage(level)
+        val levelImage = getLevelImage(level)
         levelImage?.let { painterResource(id = it) }?.let {
             Image(
                 painter = it,
                 contentDescription = null,
                 modifier = modifier
                     .fillMaxSize()
-                    .background(shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp),color = Color(0xFF252528))
-
+                    .background(
+                        shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp),
+                        brush = Brush.linearGradient(
+                            colorStops = arrayOf(
+                                0.15f to Color(0xFF121413),
+                                1.0f to Color(0xFF241705)
+                            ),
+                        )
+                    )
             )
         }
 
@@ -61,5 +69,5 @@ fun InstagramCard(nickname: String,level:Int,modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun InstagramCardPreview() {
-    InstagramCard("김감자",1)
+    InstagramCard("김감자", 1)
 }
