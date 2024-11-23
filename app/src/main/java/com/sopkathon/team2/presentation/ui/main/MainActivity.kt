a1640f55fabd6091505e0c8bbbb4e7aa604a2f99
serioushyeon <zz64446949@gmail.com>
2024-11-24 05:29:55 +0900

    [chore] main home뷰로 복구

--- app/src/main/java/com/sopkathon/team2/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopkathon/team2/presentation/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sopkathon.team2.presentation.ui.main
+package com.sopkathon.team2.presentation.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.sopkathon.team2.presentation.ui.CompleteScreen
+import com.sopkathon.team2.presentation.ui.home.HomeScreen
 import com.sopkathon.team2.ui.theme.ANDSOPTSOPKATHONTEAM2Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ANDSOPTSOPKATHONTEAM2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CompleteScreen(modifier = Modifier.padding(innerPadding))
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
